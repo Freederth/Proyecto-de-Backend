7d22ffc40e7c043e6eb488a9f10404932bc335eb
Isabel Achurra <iachurra@ondasonora.cl>
2022-11-02 15:39:10 -0300

    arreglos

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const passport = require("./src/utils/passportMiddleware");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 const PORT = process.env.PORT || 8080;
 
